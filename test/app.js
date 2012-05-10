6e3925543f20b4f513046f38547bd1b5a4d9d8ec
James O'Reilly <james@jamesor.com>
2012-05-10 15:08:23 -0400

    Added support for "javascripts' and 'stylesheets' folder names to follow Express' default setup.

--- test/app.js
+++ test/app.js
@@ -13,14 +13,33 @@ var resminConfig = {
             "/js/baz.js"
         ]
     },
+    javascripts: {
+        "all": [
+            "/js/foo.js",
+            "/js/bar.js",
+            "/js/baz.js"
+        ]
+    },
     css: {
         "all": [
+            "//ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js",
+            "//ajax.googleapis.com/ajax/libs/jqueryui/1.8.13/jquery-ui.min.js",
             "//fonts.googleapis.com/css?family=Indie+Flower:regular&v1",
             "//fonts.googleapis.com/css?family=Inconsolata:regular&v1",
             "/css/foo.css",
             "/css/bar.css",
             "/css/baz.less",
 			"/css/qux.styl"
+        ]
+    },
+    stylesheets: {
+        "all": [
+            "//fonts.googleapis.com/css?family=Indie+Flower:regular&v1",
+            "//fonts.googleapis.com/css?family=Inconsolata:regular&v1",
+            "/css/foo.css",
+            "/css/bar.css",
+            "/css/baz.less",
+            "/css/qux.styl"
         ]
     }
 };
@@ -47,43 +66,79 @@ browser.get('/', function (res, $){
     res.should.have.status(200);
     res.should.not.have.status(500);
     res.should.have.header('Content-Type', 'text/html; charset=utf-8');
-    
+
     var css = $("ul.css"),
-        js = $("ul.js");
-        
+        stylesheets = $("ul.stylesheets"),
+        js = $("ul.js"),
+        javascripts = $("ul.javascripts");
+
     css.should.have.many("li");
+    stylesheets.should.have.many("li");
     js.should.have.many("li");
-    
+    javascripts.should.have.many("li");
+
 	console.log("");
-    console.log("CSS files:");
+    console.log("css files:");
     css.find("li").each(function(i,elm){
         console.log(elm.innerHTML);
     });
-    
+
+    console.log("");
+    console.log("stylesheets files:");
+    stylesheets.find("li").each(function(i,elm){
+        console.log(elm.innerHTML);
+    });
+
 	console.log("");
-    console.log("JavaScript files:");
+    console.log("js files:");
     js.find("li").each(function(i,elm){
         console.log(elm.innerHTML);
     });
-	
+
+    console.log("");
+    console.log("javaScripts files:");
+    javascripts.find("li").each(function(i,elm){
+        console.log(elm.innerHTML);
+    });
+
 	console.log("");
 	browser.get("/css/baz.less", function (res, $) {
 		res.should.have.status(200);
 		res.should.not.have.status(500);
 		res.should.have.header('Content-Type', 'text/css');
-		console.log("Real-time compilation of LESS: Passed!");
-		
+		console.log("Real-time compilation of LESS in 'css' folder: Passed!");
+
 		console.log("");
 		browser.get("/css/qux.styl", function (res, $) {
 			res.should.have.status(200);
 			res.should.not.have.status(500);
 			res.should.have.header('Content-Type', 'text/css');
-			console.log("Real-time compilation of Stylus: Passed!");
-			
+			console.log("Real-time compilation of Stylus in 'css' folder: Passed!");
+
 			console.log("");
 			console.log("All tests passed!");
 			app.close();
 		});
 	});
+
+    console.log("");
+    browser.get("/stylesheets/baz.less", function (res, $) {
+        res.should.have.status(200);
+        res.should.not.have.status(500);
+        res.should.have.header('Content-Type', 'text/css');
+        console.log("Real-time compilation of LESS in 'stylesheets' folder: Passed!");
+
+        console.log("");
+        browser.get("/stylesheets/qux.styl", function (res, $) {
+            res.should.have.status(200);
+            res.should.not.have.status(500);
+            res.should.have.header('Content-Type', 'text/css');
+            console.log("Real-time compilation of Stylus in 'stylesheets' folder: Passed!");
+
+            console.log("");
+            console.log("All tests passed!");
+            app.close();
+        });
+    });
 });
 
